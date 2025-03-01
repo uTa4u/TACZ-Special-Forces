6532eb8539315c7798fc5890b0a47662387cd5ca
uTa4u <63360499+uTa4u@users.noreply.github.com>
2025-03-01 13:12:44 +0300

    Working specialty assignment

--- src/main/java/su/uTa4u/specialforces/entities/TestEntity.java
+++ src/main/java/su/uTa4u/specialforces/entities/TestEntity.java
@@ -1,22 +1,28 @@
 package su.uTa4u.specialforces.entities;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.AnimationState;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.ForgeEventFactory;
+import org.jetbrains.annotations.Nullable;
 import su.uTa4u.specialforces.SpecialForces;
 import su.uTa4u.specialforces.Specialty;
-import su.uTa4u.specialforces.Util;
 
 public class TestEntity extends PathfinderMob {
+
+    private static final Attribute[] ATTRIBUTES;
 
     private static final EntityDataAccessor<Specialty> SPECIALTY = SynchedEntityData.defineId(TestEntity.class, ModEntityDataSerializers.SPECIAL_FORCE_SPECIALTY);
     public final AnimationState idleAnimationState = new AnimationState();
@@ -29,36 +35,34 @@ public class TestEntity extends PathfinderMob {
     public TestEntity(Level level, Specialty specialty, double x, double y, double z) {
         this(ModEntities.TEST_ENTITY.get(), level);
         this.setSpecialty(specialty);
+        SpecialForces.LOGGER.info("Spec in constr was set to: " + this.getSpecialty());
         this.setPos(x, y, z);
     }
 
+    @Nullable
     @Override
-    public void onAddedToWorld() {
-        Level level = this.level();
-        MobSpawnType reason = this.getSpawnType();
-
-        // TODO: reason == MobSpawnType.SPAWN_EGG
-        if (reason == MobSpawnType.SPAWNER || reason == MobSpawnType.COMMAND) {
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor levelAccessor, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag) {
+        // TODO: spawnType == MobSpawnType.SPAWN_EGG
+        if (spawnType == MobSpawnType.SPAWNER || spawnType == MobSpawnType.COMMAND) {
             this.setSpecialty(Specialty.getRandomSpecialty());
+            SpecialForces.LOGGER.info("Random Spec: " + this.getSpecialty());
         }
+
+        ServerLevel level = levelAccessor.getLevel();
 
         if (!level.isClientSide) this.registerSpecialGoals();
-        
-        Util.copyAttributes(this.getAttributes(), this.getSpecialty().getAttributes());
-        this.setHealth(this.getMaxHealth());
+        this.copySpecialAttributes();
 
-        if (this.getSpecialty() == Specialty.COMMANDER) {
-            // Spawn allies based on the current mission
-            Vec3 pos = this.position();
-            // TODO: This even if entity was loaded from disk, not freshly created. Not the worst, but ideally should
-            //  only run once - on freshly created.
-            //  maybe finalizeSpawn should only process Commander (natural spawn) and other spawns
-            //  should be processed (set specialty, goals, attributes) in the custom constructor?
-            level.addFreshEntity(new TestEntity(level, Specialty.SNIPER, pos.x + 1, pos.y, pos.z + 1));
-            level.addFreshEntity(new TestEntity(level, Specialty.MEDIC, pos.x - 1, pos.y, pos.z - 1));
-        }
+        if (this.getSpecialty() != Specialty.COMMANDER) return spawnData;
 
-        super.onAddedToWorld();
+        // TODO: Spawn allies based on the current mission
+        Vec3 pos = this.position();
+        TestEntity mob1 = new TestEntity(level, Specialty.SNIPER, pos.x + 1, pos.y, pos.z + 1);
+//            TestEntity mob2 = new TestEntity(level, Specialty.MEDIC, pos.x - 1, pos.y, pos.z - 1);
+        ForgeEventFactory.onFinalizeSpawn(mob1, level, difficulty, MobSpawnType.BREEDING, spawnData, dataTag);
+        level.addFreshEntity(mob1);
+
+        return spawnData;
     }
 
     @Override
@@ -129,17 +133,20 @@ public class TestEntity extends PathfinderMob {
     }
 
     public static AttributeSupplier.Builder createDefaultAttributes() {
-        return PathfinderMob.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 1)
-                .add(Attributes.FOLLOW_RANGE, 1)
-                .add(Attributes.KNOCKBACK_RESISTANCE, 1)
-                .add(Attributes.MOVEMENT_SPEED, 1)
-                .add(Attributes.FLYING_SPEED, 1)
-                .add(Attributes.ATTACK_DAMAGE, 1)
-                .add(Attributes.ATTACK_KNOCKBACK, 1)
-                .add(Attributes.ATTACK_SPEED, 1)
-                .add(Attributes.ARMOR, 1)
-                .add(Attributes.ARMOR_TOUGHNESS, 1);
+        AttributeSupplier.Builder builder = PathfinderMob.createLivingAttributes();
+        for (Attribute attribute : ATTRIBUTES) builder.add(attribute, 1);
+        return builder;
+    }
+
+    private void copySpecialAttributes() {
+        AttributeSupplier src = this.getSpecialty().getAttributes();
+        for (Attribute attribute : ATTRIBUTES) {
+            AttributeInstance attributeInstance = this.getAttributes().getInstance(attribute);
+            if (attributeInstance != null && src.hasAttribute(attribute)) {
+                attributeInstance.setBaseValue(src.getBaseValue(attribute));
+            }
+        }
+        this.setHealth(this.getMaxHealth());
     }
 
     @Override
@@ -159,5 +166,20 @@ public class TestEntity extends PathfinderMob {
 
     private void setSpecialty(Specialty specialty) {
         this.entityData.set(SPECIALTY, specialty);
+    }
+
+    static {
+        ATTRIBUTES = new Attribute[]{
+                Attributes.MAX_HEALTH,
+                Attributes.FOLLOW_RANGE,
+                Attributes.KNOCKBACK_RESISTANCE,
+                Attributes.MOVEMENT_SPEED,
+                Attributes.FLYING_SPEED,
+                Attributes.ATTACK_DAMAGE,
+                Attributes.ATTACK_KNOCKBACK,
+                Attributes.ATTACK_SPEED,
+                Attributes.ARMOR,
+                Attributes.ARMOR_TOUGHNESS,
+        };
     }
 }
