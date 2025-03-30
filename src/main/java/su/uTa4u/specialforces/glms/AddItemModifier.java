0da778f266f89c1b999203a604ede0af18779f39
uTa4u <63360499+uTa4u@users.noreply.github.com>
2025-03-30 22:31:45 +0300

    Somewhat working extension of add_item modifier

--- src/main/java/su/uTa4u/specialforces/glms/AddItemModifier.java
+++ src/main/java/su/uTa4u/specialforces/glms/AddItemModifier.java
@@ -1,13 +1,10 @@
 package su.uTa4u.specialforces.glms;
 
 import com.google.common.base.Suppliers;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.TagParser;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
@@ -16,34 +13,24 @@ import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class AddItemModifier extends LootModifier {
     public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(() ->
             RecordCodecBuilder.create((inst) ->
                     LootModifier.codecStart(inst).and(
-                            inst.group(
-                                    ForgeRegistries.ITEMS.getCodec().fieldOf("item").forGetter(m -> m.item),
-                                    Codec.INT.fieldOf("count").forGetter(m -> m.count),
-                                    Codec.STRING.fieldOf("nbt").forGetter(m -> m.nbt)
-                            )
+                            Entry.CODEC.listOf().fieldOf("entries").forGetter(m -> m.entries)
                     ).apply(inst, AddItemModifier::new)
             )
     );
 
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private final List<Entry> entries;
 
-    private final Item item;
-    private final int count;
-    private final String nbt;
-
-    public AddItemModifier(LootItemCondition[] conditions, Item item, int count, String nbt) {
+    public AddItemModifier(LootItemCondition[] conditions, List<Entry> entries) {
         super(conditions);
-        this.item = item;
-        this.count = count;
-        this.nbt = nbt;
+        this.entries = entries;
     }
 
     @NotNull
@@ -55,15 +42,9 @@ public class AddItemModifier extends LootModifier {
             }
         }
 
-        CompoundTag nbt;
-        try {
-            nbt = TagParser.parseTag(this.nbt);
-        } catch (CommandSyntaxException e) {
-            nbt = null;
-            LOGGER.error(e.getMessage());
+        for (Entry entry : this.entries) {
+            generatedLoot.add(new ItemStack(entry.item, entry.count, entry.nbt));
         }
-
-        generatedLoot.add(new ItemStack(this.item, this.count, nbt));
 
         return generatedLoot;
     }
@@ -71,5 +52,28 @@ public class AddItemModifier extends LootModifier {
     @Override
     public Codec<? extends IGlobalLootModifier> codec() {
         return CODEC.get();
+    }
+
+    public static class Entry {
+        public static final Codec<Entry> CODEC = RecordCodecBuilder.create((inst) ->
+                        inst.group(
+                                ForgeRegistries.ITEMS.getCodec().fieldOf("item").forGetter(m -> m.item),
+                                Codec.intRange(1, 64).optionalFieldOf("count", 1).forGetter(m -> m.count),
+                                CompoundTag.CODEC.optionalFieldOf("nbt", new CompoundTag()).forGetter(m -> m.nbt),
+                                Codec.floatRange(0.0f, 1.0f).optionalFieldOf("chance", 1.0f).forGetter(m -> m.chance)
+                        ).apply(inst, Entry::new)
+                );
+
+        private final Item item;
+        private final int count;
+        private final CompoundTag nbt;
+        private final float chance;
+
+        public Entry(Item item, int count, CompoundTag nbt, float chance) {
+            this.item = item;
+            this.count = count;
+            this.nbt = nbt;
+            this.chance = chance;
+        }
     }
 }
